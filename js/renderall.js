b55ed9c42d8f5bb068b946ce22be765836f9a3a9
emi-ak <298037097+emi-ak@users.noreply.github.com>
2026-07-01 21:46:36 -0400

    Update renderall.js

--- js/renderall.js
+++ js/renderall.js
@@ -1,4 +1,16 @@
+import { getData } from "./state.js";
+
+import { renderDashboard } from "./dashboard.js";
+import { renderActivities } from "./activities.js";
+import { renderTimeline } from "./timeline.js";
+import { renderModules } from "./coursework.js";
+import { renderVision } from "./vision.js";
+import { renderAffirmations } from "./affirmation.js";
+import { renderResources } from "./resources.js";
+import { renderGoals } from "./todo.js";
+
 export function renderAll() {
+  const data = getData();
   if (!data) return;
 
   renderDashboard();
