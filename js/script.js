f2e1c07661fc4b73a3ddbdda00174dda880d263e
emi-ak <298037097+emi-ak@users.noreply.github.com>
2026-07-01 20:47:09 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,18 @@ import { startAuth } from "./js/auth.js";
 import { setCurrentUser, loadPlanner, savePlanner } from "./js/database.js";
 import { defaultData, todayISO } from "./js/data.js";
 
+import "./js/navigation.js";
+import "./js/theme.js";
+import "./js/forms.js";
+import "./js/dashboard.js";
+import "./js/activities.js";
+import "./js/coursework.js";
+import "./js/timeline.js";
+import "./js/vision.js";
+import "./js/affirmations.js";
+import "./js/resources.js";
+import "./js/goals.js";
+
 let data = null;
 let classificationVisible = false;
 
